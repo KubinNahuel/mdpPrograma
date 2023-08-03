39e8bc092904dfc7a8563f6390384b383170f478
KubinNahuel <rn.kubin@hotmail.com>
2023-08-03 20:25:02 -0300

    carpeta react

---
+++ react/ClaseReactNuevo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import {PrimerComponente} from './components/primerComponente';
+function App() {
+ 
+  return (
+    
+        <PrimerComponente/>
+  );
+}
+
+export default App;
