2479f89b184ad77b487752d6fe807b0422f67b62
KubinNahuel <rn.kubin@hotmail.com>
2023-08-09 19:43:55 -0300

    error de subida react

---
+++ ClaseReact 1b/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {NavBar, Footer} from './components/navBar'
+
+
+function App() {
+  return (
+    <>
+   
+     <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ ClaseReact 1b/src/components/navBar.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function NavBar(props){
+   
+    return(
+        <>
+           <h1>{props.persona.edad}</h1>
+        </>
+    )
+}
+
+export function Footer(){
+    const [estado, setEstado]= useState("");
+    const persona={
+        name:"rodrigo",
+        edad: 35
+    };
+    const alerta=()=>{
+        alert("se envio")
+    }
+    const cambiar=(e)=>{
+        setEstado(e)
+    }
+    return(
+        <>
+       
+        <NavBar persona={persona}/>
+        <button onClick={alerta}>Enviar</button>
+        <input onChange={(e)=> cambiar(e.target.value)}></input>
+        <h1>{estado}</h1>
+        </>
+    )
+}
+export default NavBar
